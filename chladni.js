cd21eef2fc4d0f757be011626954e57c8a9cfabb
siholla <simonholmlar@gmail.com>
2025-04-18 19:31:46 +0200

    Add files via upload

--- chladni.js
+++ chladni.js
@@ -1,84 +1,146 @@
+let particles, sliders, m, n, a, b;
 
-let particles = [];
-let sliders = {};
-let m, n, a, b, v;
-
+// vibration strength params
 let A = 0.02;
 let minWalk = 0.002;
 
 const settings = {
-  nParticles: 20000,
-  canvasSize: [600, 600],
-  drawHeatmap: false
-};
+  nParticles : 20000,
+  canvasSize : [600, 600],
+  drawHeatmap : false
+}
 
-const pi = Math.PI;
+const pi = 3.1415926535;
 
-const chladni = (x, y, a, b, m, n) =>
-  a * Math.sin(pi * n * x) * Math.sin(pi * m * y) + b * Math.sin(pi * m * x) * Math.sin(pi * n * y);
+// chladni 2D closed-form solution - returns between -1 and 1
+const chladni = (x, y, a, b, m, n) => 
+  a * sin(pi*n*x) * sin(pi*m*y) + b * sin(pi*m*x) * sin(pi*n*y);
 
-function setup() {
+/* Initialization */
+
+const DOMinit = () => {
   let canvas = createCanvas(...settings.canvasSize);
   canvas.parent('sketch-container');
-  pixelDensity(1);
-  background(0);
 
+  // sliders
   sliders = {
-    m: select('#mSlider'),
-    n: select('#nSlider'),
-    a: select('#aSlider'),
-    b: select('#bSlider'),
-    v: select('#vSlider'),
-    num: select('#numSlider'),
-  };
-
-  sliders.num.input(() => {
-    settings.nParticles = parseInt(sliders.num.value());
-    setupParticles(); // reinitialize particles
-  });
-
-  setupParticles();
+    m : select('#mSlider'), // freq param 1
+    n : select('#nSlider'), // freq param 2
+    a : select('#aSlider'), // freq param 3
+    b:  select('#bSlider'), // freq param 4
+    v : select('#vSlider'), // velocity
+    num : select('#numSlider'), // number
+  }
 }
 
-function setupParticles() {
+const setupParticles = () => {
+  // particle array
   particles = [];
   for (let i = 0; i < settings.nParticles; i++) {
-    particles.push(new Particle());
+    particles[i] = new Particle();
   }
 }
 
-function draw() {
-  m = float(sliders.m.value());
-  n = float(sliders.n.value());
-  a = float(sliders.a.value());
-  b = float(sliders.b.value());
-  v = float(sliders.v.value());
 
-  noStroke();
-  fill(255);
-  for (let p of particles) {
-    p.move();
-    p.show();
-  }
-}
+/* Particle dynamics */
 
 class Particle {
+
   constructor() {
-    this.x = random();
-    this.y = random();
+    this.x = random(0,1);
+    this.y = random(0,1);
+    this.stochasticAmplitude;
+
+    // this.color = [random(100,255), random(100,255), random(100,255)];
+    
+    this.updateOffsets();
   }
 
   move() {
+    // what is our chladni value i.e. how much are we vibrating? (between -1 and 1, zeroes are nodes)
     let eq = chladni(this.x, this.y, a, b, m, n);
-    this.x += random(-v, v) * eq;
-    this.y += random(-v, v) * eq;
-    this.x = constrain(this.x, 0, 1);
-    this.y = constrain(this.y, 0, 1);
+
+    // set the amplitude of the move -> proportional to the vibration
+    this.stochasticAmplitude = v * abs(eq);
+
+    if (this.stochasticAmplitude <= minWalk) this.stochasticAmplitude = minWalk;
+
+    // perform one random walk
+    this.x += random(-this.stochasticAmplitude, this.stochasticAmplitude);
+    this.y += random(-this.stochasticAmplitude, this.stochasticAmplitude);
+ 
+    this.updateOffsets();
+  }
+
+  updateOffsets() {
+    // handle edges
+    if (this.x <= 0) this.x = 0;
+    if (this.x >= 1) this.x = 1;
+    if (this.y <= 0) this.y = 0;
+    if (this.y >= 1) this.y = 1;
+
+    // convert to screen space
+    this.xOff = width * this.x; // (this.x + 1) / 2 * width;
+    this.yOff = height * this.y; // (this.y + 1) / 2 * height;
   }
 
   show() {
-    let px = this.x * width;
-    let py = this.y * height;
-    rect(px, py, 1, 1);
+    // stroke(...this.color);
+    point(this.xOff, this.yOff)
   }
+}
+
+const moveParticles = () => {
+  let movingParticles = particles.slice(0, N);
+
+  // particle movement
+  for(let particle of movingParticles) {
+    particle.move();
+    particle.show();
+  }
+}
+
+const updateParams = () => {
+  m = sliders.m.value();
+  n = sliders.n.value();
+  a = sliders.a.value();
+  b = sliders.b.value()
+  v = sliders.v.value();
+  N = sliders.num.value();
+}
+
+const drawHeatmap = () => {
+  // draw the function heatmap in the background (not working)
+  if (settings.drawHeatmap) {
+    let res = 3;
+    for (let i = 0; i <= width; i+=res) {
+      for (let j = 0; j <= height; j+=res) {
+        let eq = chladni(i/width, j/height, a, b, m, n);
+        noStroke();
+        fill((eq + 1) * 127.5);
+        square(i, j, res);
+      }
+    }
+  }
+}
+
+const wipeScreen = () => {
+  background(30);
+  stroke(255);
+}
+
+
+/* Timing */
+
+// run at DOM load
+function setup() {
+  DOMinit();
+  setupParticles();
+}
+// run each frame
+function draw() {
+  wipeScreen();
+  updateParams();
+  drawHeatmap();
+  moveParticles();
 }
